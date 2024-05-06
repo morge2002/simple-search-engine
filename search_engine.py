aa15376e2a18e5af538f1ec462ed8927c3706e5e
MorganLewis <sc20mcl@leeds.ac.uk>
2024-05-06 12:04:18 +0100

    multiple word rank by tf_idf of query word

--- search_engine.py
+++ search_engine.py
@@ -15,10 +15,15 @@ class SearchEngine:
             word = self.indexer.parse_word(word)
             if word not in self.indexer.word_index:
                 continue
+
+            tf_idf = self.query_word_tf_idf(word, query_words)
+
+            # Calculate rank for each page for the word
             for page_id in self.indexer.word_index[word].keys():
                 if page_id not in ranks:
                     ranks[page_id] = 0
-                ranks[page_id] += self.calculate_rank(page_id, word)
+                # Rank is multiplied by the tf_idf score of the word in the query
+                ranks[page_id] += self.calculate_rank(page_id, word) * tf_idf
 
         if not ranks:
             print("No results found.")
@@ -39,6 +44,11 @@ class SearchEngine:
         # tf_idf = tf * idf
         # return tf_idf
         return self.indexer.word_index[word][page_id]
+
+    def query_word_tf_idf(self, word, query_words):
+        tf = query_words.count(word) / len(query_words)
+        idf = math.log(len(self.indexer.url_to_id.keys()) / len(self.indexer.word_index[word].keys()))
+        return tf * idf
 
 
 if __name__ == "__main__":
